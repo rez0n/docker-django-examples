f315e9a48aa00bcee8cc57b8a27336f44b44c0ac
Denis Verbin <den.verbin@gmail.com>
2022-08-21 01:46:57 +0300

    added mysqlclient example

--- app/settings.py
+++ app/settings.py
@@ -50,8 +50,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'example',
+        'USER': 'example',
+        'PASSWORD': 'example',
+        'HOST': 'db',
+        'PORT': 3306,
     }
 }
 
